bad3b058b12df9d63264fd2ccf50624b839a5826
Arkendu <108846361+arkendu-porel@users.noreply.github.com>
2023-09-27 08:50:47 +0530

    Update module_3_task_1_string_manipulation.php

--- module_3_task_1_string_manipulation.php
+++ module_3_task_1_string_manipulation.php
@@ -1,5 +1,9 @@
 <?php
+   function stringManipulate($string) {
+      $textNew = str_replace("brown", "red", strtolower($string));
+      echo $textNew;
+   }
+
    $text = "The quick brown fox jumps over the lazy dog.";
-   $textNew = str_replace("brown", "red", strtolower($text));
-   echo $textNew;
+   stringManipulate($text);
 ?>
