c04586f4f3707a3a11d018242abd7bf7d0240bcb
Arkendu <108846361+arkendu-porel@users.noreply.github.com>
2023-09-28 20:05:55 +0530

    Update module_3_task_4_multidimention.php

--- module_3_task_4_multidimention.php
+++ module_3_task_4_multidimention.php
@@ -1,8 +1,26 @@
 <?php
-   function avgGrade($studentGrades) {
-      $grade1 = round(array_sum($studentGrades['student1'])/3);
-      $grade2 = round(array_sum($studentGrades['student2'])/3);
-      $grade3 = round(array_sum($studentGrades['student3'])/3);
+   function findGrade($marks){
+      if ($marks<100 && $marks>95) {
+         $grade =  "AA";
+      } 
+      elseif ($marks<95 && $marks>85) {
+         $grade =  "A";
+      }
+      elseif ($marks<85 && $marks>75) {
+         $grade =  "B";
+      }
+      elseif ($marks<75 && $marks>65) {
+         $grade =  "C";
+      }
+      return $grade;    
+   }
+   function avgGrade($studentGrades){
+      $marks1 = round(array_sum($studentGrades['student1'])/3);
+      $marks2 = round(array_sum($studentGrades['student2'])/3);
+      $marks3 = round(array_sum($studentGrades['student3'])/3);
+      $grade1 = findGrade($marks1);
+      $grade2 = findGrade($marks2);
+      $grade3 = findGrade($marks3);
       echo "First student grade is {$grade1} \n";
       echo "Second student grade is {$grade2} \n";
       echo "Third student grade is {$grade3}";
@@ -19,9 +37,9 @@
          'Science' => 88
       ],
       'student3' => [
-         'Math' => 86,
+         'Math' => 79,
          'English' => 73,
-         'Science' => 83
+         'Science' => 77
       ],
    ];
    avgGrade($studentGrades);
